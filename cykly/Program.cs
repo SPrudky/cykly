1b667faa9ed9c83214b189a15801dc487ca9e94a
Samuel Prudký <Prudky.Samuel@stud.vassboskovice.cz>
2021-11-09 10:36:36 +0100

    Add project files.

---
+++ cykly/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cykly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
